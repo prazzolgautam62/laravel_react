fa45fb00c711e1bd2f7d0dd6dfe0f5de9313d6db
Prajwol Gautam <prazzolgautam@gmail.com>
2023-12-03 21:51:54 +0545

    delete user

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -1,24 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import Layout from "../components/Layout"
 import Header from '../components/Header'
-import { _getUsers } from '../services/user';
+import { _deleteUser, _getUsers } from '../services/user';
 import { Table, Button } from 'react-bootstrap';
 
 function Home() {
 
     const [users, setUsers] = useState([]);
 
+    const handleDelete = async (id) => {
+        if (!confirm("Are you sure you want to delete the user ?")) return;
+        try {
+            const user = await _deleteUser(id);
+            fetchData();
+        } catch (error) {
+            console.error('Error deleting user:', error);
+        }
+    };
+
+    const fetchData = async () => {
+        try {
+            const userData = await _getUsers();
+            setUsers(userData.data.data);
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        }
+    };
+
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const userData = await _getUsers();
-
-                setUsers(userData.data.data);
-            } catch (error) {
-                console.error('Error fetching user data:', error);
-            }
-        };
-
         fetchData();
     }, []);
 
@@ -39,9 +48,9 @@ function Home() {
                         </tr>
                     </thead>
                     <tbody>
-                        {users.map((user,key) => (
+                        {users.map((user, key) => (
                             <tr key={user.id}>
-                                <td>{key+1}</td>
+                                <td>{key + 1}</td>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
                                 <td>{(user.created_at)}</td>
