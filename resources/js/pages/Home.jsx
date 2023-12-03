bbf92537acacb22c04efac929085d1e383f02e44
Prajwol Gautam <prazzolgautam@gmail.com>
2023-12-03 22:31:16 +0545

    user add and update

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -1,12 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import Layout from "../components/Layout"
 import Header from '../components/Header'
-import { _deleteUser, _getUsers } from '../services/user';
+import { _createUser, _deleteUser, _getUsers, _updateUser } from '../services/user';
 import { Table, Button } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 function Home() {
 
     const [users, setUsers] = useState([]);
+    const [show, setShow] = useState(false);
+    const [isEdit, setIsEdit] = useState(false);
+
+    const [formData, setFormData] = useState({
+        id: null,
+        name: '',
+        email: '',
+    });
+
+    const handleClose = () => {
+        setIsEdit(false);
+        setFormData({id: null, name: '', email: ''})
+        setShow(false);
+    };
 
     const handleDelete = async (id) => {
         if (!confirm("Are you sure you want to delete the user ?")) return;
@@ -15,6 +31,39 @@ function Home() {
             fetchData();
         } catch (error) {
             console.error('Error deleting user:', error);
+        }
+    };
+
+    const handleAdd = async () => {
+        setIsEdit(false);
+        setShow(true);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(); 
+        try{
+            const user = await _createUser(formData);
+            handleClose();
+            fetchData();
+        } catch(error){
+            console.error('Error adding user:', error);
+        }
+    };
+
+    const handleEdit = async (user) => {
+        setFormData({id: user.id, name: user.name, email: user.email});
+        setIsEdit(true);
+        setShow(true);
+    };
+
+    const handleUpdate = async (e) => {
+        e.preventDefault(); 
+        try{
+            const user = await _updateUser(formData.id,formData);
+            handleClose();
+            fetchData();
+        } catch(error){
+            console.error('Error updating user:', error);
         }
     };
 
@@ -37,7 +86,8 @@ function Home() {
             <Header />
             <div className="container">
                 <h2 className="text-center mt-5 mb-3">Home Page</h2>
-                <Table striped bordered hover>
+                <Button variant="outline-success" onClick={() => handleAdd()}>Add User</Button>{' '}
+                <Table striped bordered hover className='mt-2'>
                     <thead>
                         <tr>
                             <th>#</th>
@@ -55,7 +105,7 @@ function Home() {
                                 <td>{user.email}</td>
                                 <td>{(user.created_at)}</td>
                                 <td>
-                                    <Button variant="primary" onClick={() => handleEdit(user.id)}>
+                                    <Button variant="primary" onClick={() => handleEdit(user)}>
                                         Edit
                                     </Button>{' '}
                                     <Button variant="danger" onClick={() => handleDelete(user.id)}>
@@ -66,6 +116,38 @@ function Home() {
                         ))}
                     </tbody>
                 </Table>
+                <Modal show={show} onHide={handleClose}>
+                    <Form>
+                        <Modal.Header closeButton>
+                            <Modal.Title>{isEdit ? 'Edit User' : 'Add User'}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form.Group className="mb-3" controlId="formBasicName">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="text" placeholder="Enter name" value={formData.name}
+                                    onChange={(e) =>
+                                        setFormData({ ...formData, name: e.target.value })
+                                    }
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" value={formData.email}
+                                    onChange={(e) =>
+                                        setFormData({ ...formData, email: e.target.value })
+                                    } />
+                            </Form.Group>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                            <Button type="Submit" variant="primary" onClick={isEdit ? handleUpdate : handleSubmit}>
+                                {isEdit ? 'Update' : 'Submit'}
+                            </Button>
+                        </Modal.Footer>
+                    </Form>
+                </Modal>
             </div>
         </Layout>
     );
